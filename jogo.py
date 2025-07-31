c1875006f59a1c8dc7943ec5717fd1a8e81e3a38
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-07-31 14:51:09 +0000

    segunda parte do jogo

--- jogo.py
+++ jogo.py
@@ -2,8 +2,23 @@ import random
 print("☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎")
 print("✝︎✝︎ jogo de adivinhacao ✝︎✝︎")
 print("☘︎        kauan         ☘︎" )
-print("⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎⚛︎" )
+print("☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎☯︎")
 
 numeroSecreto= random.randrange(0, 100)
 totalTentativas = 0
 pontos = 100
+
+print(" Qual níveis de dificuldade? ")
+print("(1)- Fácil (2)- Médio (3)- Difícil ")
+
+nível = int(input("Escolha um nível: "))
+
+if(nível == 1) :
+    print("20 tentativas.")
+    totalTentativas = 20 
+elif(nível == 2) :
+    print("10 tentativas.")
+    totalTentativas = 10
+elif(nível == 3) :
+    print("5 tentativas.")
+    totalTentativas = 5
